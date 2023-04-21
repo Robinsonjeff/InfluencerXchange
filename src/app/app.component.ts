5a6f38a37f7695aa7d21781af5164f2e32db4296
Joel <joel@Joels-MacBook-Pro.local>
2023-04-21 13:28:37 -0500

    made nav-bar, made blur text, made basic influencer page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'InfluencerXchange';
+
+
+  //get from mongoDB
+  influencers = [
+    {
+      name:"Crob",
+      postPrice: 300,
+      targetAudience:"Gamers",
+      socialMediaLink:"https://twitter.com/MrBeast"
+    },
+    {
+      name:"Joel",
+      postPrice: 100,
+      targetAudience:"Test",
+      socialMediaLink:"https://twitter.com/ElonMusk"
+    },
+    {
+      name:"Scott",
+      postPrice: 1000,
+      targetAudience:"Fashion",
+      socialMediaLink:"https://twitter.com/JoeBiden"
+    },
+    {
+      name:"Murrell",
+      postPrice: 20000,
+      targetAudience:"Test2",
+      socialMediaLink:"https://twitter.com/JoeRogan"
+    }
+  ]
+
+  
+  goToSocialSite(link:string){
+    window.open(link)
+  }
+
+
 }
