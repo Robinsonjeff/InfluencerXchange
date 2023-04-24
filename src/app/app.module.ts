f573c676951523c63094d680b30273da388af204
CRob <coletista7@gmail.com>
2023-04-24 13:42:56 -0500

    Updated registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { AccountPageComponent } from './account-page/account-page.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    AccountPageComponent
-
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { InfluencersComponent } from './influencers/influencers.component';
 import { AdvertisersComponent } from './advertisers/advertisers.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import {MatCard, MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+
 const appRoutes: Routes = [
   { path: 'influencers', component: InfluencersComponent },
   { path: 'advertisers', component: AdvertisersComponent },
@@ -24,12 +26,24 @@ const appRoutes: Routes = [
     AppComponent,
     NavBarComponent,
     InfluencersComponent,
-    AdvertisersComponent
+    AdvertisersComponent,
+    FooterComponent,
+    AccountPageComponent,
+    RegisterPageComponent,
+    
 
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/register-page/register-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.css']
+})
+export class RegisterPageComponent {
+
+  
+
+  onSubmit(){
+    console.log("Submited")
+  }
+}
