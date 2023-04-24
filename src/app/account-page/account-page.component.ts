e83b263192840d0402314fded73ed7465428efe3
CRob <coletista7@gmail.com>
2023-04-24 11:23:58 -0500

    added footer

---
+++ src/app/account-page/account-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-page',
+  templateUrl: './account-page.component.html',
+  styleUrls: ['./account-page.component.css']
+})
+export class AccountPageComponent {
+
+}
